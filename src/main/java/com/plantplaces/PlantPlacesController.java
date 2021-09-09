049c4ccf427733e101e1a757ed4618d442b1e31c
Justin Tran <tranjg@mail.uc.edu>
2021-09-09 14:40:30 -0400

    Update / endpoints

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,11 +3,25 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
+/**
+ * Handle the /start endpoint
+ * @return
+ *
+ */
 @Controller
 public class PlantPlacesController {
 	
 	@RequestMapping("/start")
 	public String start() {
+		return "start";
+	}
+	
+	
+	
+	@RequestMapping("/")
+	public String index() {
 		return "start";
 	}
 
